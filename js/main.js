ad93b5132fe9a1c0071a78e029f665f41909f3e9
felipemaxplay <felipemaxplay@gmail.com>
2023-01-16 12:19:43 -0300

    test fetch API

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World.")
+const offset = 0;
+const limit = 20;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => {
+        return response.json();
+    }
+    )
+    .then((jsonBody) => {
+        console.log(jsonBody);
+    });
