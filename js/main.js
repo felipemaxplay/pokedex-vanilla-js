d474d7019374973e2cd803fb613bb2e89e83a0f2
felipemaxplay <felipemaxplay@gmail.com>
2023-01-16 18:33:54 -0300

    created logic to pagination buttom

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 let pokemonList = document.getElementById('pokemon-list');
+let LoadMoreButton = document.getElementById('loadMore');
+const limit = 20;
+let offset = 0;
 
-function pokemonConverterLi(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.primaryType}">
-        <span class="pokemon-number">#${pokemon.order}</span>
-        <span class="pokemon-name">${pokemon.name}</span>
-        <div class="pokemon-details">
-            <ol class="pokemon-types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-    </li>`
+function LoadMorePokemons(offset, limit) {
+    PokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.primaryType}">
+            <span class="pokemon-number">#${pokemon.order}</span>
+            <span class="pokemon-name">${pokemon.name}</span>
+            <div class="pokemon-details">
+                <ol class="pokemon-types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>`).join('');
+        pokemonList.innerHTML += newHtml;
+    })
 };
 
-PokeApi.getPokemons().then((pokemons = []) => {
-    const newList = pokemons.map(pokemonConverterLi).join('');
-    pokemonList.innerHTML = newList;
+LoadMorePokemons(offset, limit);
+
+LoadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    LoadMorePokemons(offset, limit);
 });
